fe880b0153374144b15ae942d25cbac31d7c2785
Mauricio Paez <mauriciopaez@Mauricios-Air.home>
2017-01-31 18:09:13 -0500

    Added barebones description of game in controller.js

--- js/adventure-game/controller/controller.js
+++ js/adventure-game/controller/controller.js
@@ -1,1 +1,17 @@
-// alert("Hello, there!");
+/*
+Title: Murders in the Rue Morgue
+--------------------------------
+
+You are trying to solve the mystery of the murders in Rue Morgue.
+
+----------
+Game World
+----------
+
+Characters										Attributes
+----------										----------
+- You (Detective)
+
+- 
+
+*/
