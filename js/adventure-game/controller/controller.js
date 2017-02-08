c3bb043bc764b99391690e9888a9be6c7c48e983
Mauricio Paez <mauriciopaez@Mauricios-Air.home>
2017-02-07 19:40:48 -0500

    Added more items to user input parser logic.

--- js/adventure-game/controller/controller.js
+++ js/adventure-game/controller/controller.js
@@ -18,6 +18,7 @@ var start = function(){
 };
 
 var parse_input = function(){
+	// Function Variables
 	var input_string = "";
 	var action = "";
 	var world_item = "";
@@ -25,14 +26,31 @@ var parse_input = function(){
 		action: "",
 		world_item: ""
 	};
-	// RegEx variables
+
+	/*RegEx variables
+	-------------------------------------------*/
+	// Items
 	var house = /house/g;
 	var street = /(?=.*\bstreet\b).+/g;
 	var neigborhood = /(?=.*\bneighborhood\b)/g;
+	var scene = /(?=.*\bscene\b)/g;
+	var victims = /(?=.*\bvictims\b)/g;
+	var papers = /(?=.*\bpapers\b)/g;
+	var box = /(?=.*\bbox\b)/g;
+	var knife = /(?=.*\bknife\b)/g;
+	var gray_hairs = /(?=.*\bgray hairs\b)/g;
+	var gold = /(?=.*\bgold\b)/g;
+	var earring = /(?=.*\bearring.\b)/g;
+	var silver = /(?=.*\bsilver\b)/g;
+	var clothes = /(?=.*\bsilver\b)/g;
+	var lightning_rod = /(?=.*\brod\b)/g;
+
+	// Actions
 	var go_to = /^go to .+$/g;
 	var look_at = /^look at .+$/g;
 	var inspect = /^inspect .+$/g;
 	var talk_to = /^talk to .+$/g;
+	var look_for = /^look for .+$/g;
 
 	input_string = $(".user-input").val().toLowerCase();
 	input_string.replace(/\s+/g, ' ').trim(); //Delete any extra spaces in between and possibly before/after
@@ -41,34 +59,59 @@ var parse_input = function(){
 		case (go_to.test(input_string) ? input_string : ""):
 			obj.action = "go to"
 			world_item = input_string.substring(6);
-			console.log(world_item)
 			switch (world_item) {
 				case (house.test(world_item) ? world_item : ""):
-					console.log("Inside Go To House case");
 					obj.world_item = "house"
 					break;
 				case (street.test(world_item) ? world_item : ""):
 				case (neigborhood.test(world_item) ? world_item : ""):
-					console.log("Inside Go To Street case");
 					obj.world_item = "street"
 					break;
 				default:
 					obj.world_item = "unknown"
 					break;
 			}
-			console.log(obj);
-			// return obj;
 			break;
 		case (look_at.test(input_string) ? input_string : ""):
-			action = "look at"
-			console.log(obj);
-			break;
-		case inspect.test(input_string):
-			action = "inspect"
-			world_item = input_string.substring(7);
+		case (inspect.test(input_string) ? input_string : ""):
+			action = "look at";
+			world_item = input_string.substring(8);
+			switch (world_item) {
+				case (scene.test(world_item) ? world_item : ""):
+				case (victims.test(world_item) ? world_item : ""):
+					// console.log("Inside Look At Scene case");
+					obj.world_item = "scene"
+				case (papers.test(world_item) ? world_item : ""):
+				case (box.test(world_item) ? world_item : ""):
+				case (old_letters.test(world_item) ? world_item : ""):
+					// console.log("Inside Look At Scene case");
+					obj.world_item = "papers"
+				case (knife.test(world_item) ? world_item : ""):
+					// console.log("Inside Look At Scene case");
+					obj.world_item = "knife"
+				case (gray_hairs.test(world_item) ? world_item : ""):
+					// console.log("Inside Look At Scene case");
+					obj.world_item = "gray hairs"
+				case (gold.test(world_item) ? world_item : ""):
+				case (earring.test(world_item) ? world_item : ""):
+				case (silver.test(world_item) ? world_item : ""):
+				case (clothes.test(world_item) ? world_item : ""):
+					// console.log("Inside Look At Scene case");
+					obj.world_item = "valuables"
+				case (lightning_rod.test(world_item) ? world_item : ""):
+					// console.log("Inside Look At Scene case");
+					obj.world_item = "rod"
+				default:
+					obj.world_item = "unknown"
+					break;
+			}
 			break;
 		case talk_to.test(input_string):
 			action = "talk to"
+			world_item = input_string.substring(7);
+			break;
+		case look_for.test(input_string):
+			action = "look for"
 			world_item = input_string.substring(7);
 			break;
 		case "start":
@@ -80,9 +123,6 @@ var parse_input = function(){
 			world_item = input_string.substring(7);
 			break;
 	}
-	//Finish this function.
-	// It should take the input, parse it, and create an object
-	// that contains an action and an item.
 };
 
 // var evaluate_input = function(){
