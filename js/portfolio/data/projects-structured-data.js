e2de9248012640c9b95031eef8c714964c39fb8a
Mauricio Paez <cornopaez@gmail.com>
2017-03-17 10:50:35 -0400

    Added structured data for all views.

---
+++ js/portfolio/data/projects-structured-data.js
@@ -0,0 +1,65 @@
+{
+	"@context": "http://schema.org",
+    "@type": "WebPage",
+    "about": [
+    	{
+        	"@type": "Website",
+            "name": "Portfolio",
+            "description": "This website is the first project I've taken on, at least on the Web Development front. It is designed with simplicity in mind following most of the concepts I have learned on Codecademy's HTML, CSS, SASS, and AngularJS tracks.",
+            "url": "http://cornopaez.com",
+            "author": {
+                "@type": "person",
+                "name": "Mauricio Páez",
+                "sameAs": "http://cornopaez.com/#/About"
+            }
+        },
+        {
+        	"@type": "Website",
+            "name": "PYTDance",
+            "description": "This is a capstone project for Codecademy's Ready \"Freelance Web Developer\" program. It is built according to the specs provided by the course found here. It is mostly built on Flexbox, though some of the styles (though, not layouts) from Bootstrap have been inherited and expanded upon. This site is fully responsive, and works well in tablets and mobile phones.",
+            "url": "http://cornopaez.com/pages/pytdance/index.html",
+            "author": {
+                "@type": "person",
+                "name": "Mauricio Páez",
+                "sameAs": "http://cornopaez.com/#/About"
+            }
+        },
+        {
+        	"@type": "Webpage",
+            "name": "Adventure Game",
+            "description": "This game is in the vein of vintage 1980's text-based adventures. This one in particular is an adaptation of Edgar Allan Poe's \"Murders in the Rue Morgue.\"",
+            "url": "http://cornopaez.com/pages/adventure-game.html",
+            "author": [
+              {
+                  "@type": "person",
+                  "name": "Mauricio Páez",
+              	  "sameAs": "http://cornopaez.com/#/About"
+              },
+              {
+              	"@type": "person",
+              	"name": "Elizabeth Páez"
+              },
+              {
+              	"@type": "person",
+              	"name": "Edgar Allan Poe",
+                "sameAs": "https://en.wikipedia.org/wiki/Edgar_Allan_Poe"
+              }
+            ]
+        },
+        {
+        	"@type": "Website",
+            "name": "Banking Application",
+            "description": "A simple banking application. Please bank with us- No fees, no nonsense. All your moneis are belong to us! Coming soon.",
+            "author": {
+                "@type": "person",
+                "name": "Mauricio Páez",
+                "sameAs": "http://cornopaez.com/#/About"
+            }
+        }
+    ],
+    "author": {
+    	"@type": "person",
+        "name": "Mauricio Páez",
+        "sameAs": "http://cornopaez.com/#/About"
+    }
+}
