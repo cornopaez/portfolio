d9a3093e4b7c0b06ddf42263db63b264f704274a
Mauricio Paez <cornopaez@gmail.com>
2017-03-17 10:26:01 -0400

    Added test structured data for index view.

---
+++ js/portfolio/data/index-structured-data.js
@@ -0,0 +1,19 @@
+{
+	"@context": "http://schema.org",
+    "@type": "WebPage",
+    "about": {
+        "@type": "Organization",
+        "name": "Mauricio_Páez | Developer",
+        "url": "http://cornopaez.com",
+        "address": {
+        "addressLocality": "Pittsburgh",
+        "addressRegion": "PA"
+        },
+        "Description": "I'm a Pittsburgh-based developer with enough knowledge of every aspect of the stack to be dangerous (and break things)."
+    },
+    "author": {
+    	"@type": "person",
+        "name": "Mauricio Páez",
+        "sameAs": "http://cornopaez.com/#/About"
+    }
+}
