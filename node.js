2d6807faa97cf604a775651f88f23830fb7e602b
Mauricio Paez <cornopaez@gmail.com>
2017-04-25 19:02:21 -0400

    Added a different pg interface as backup.

--- node.js
+++ node.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pgp = require('pg-promise')();
+var db = pgp(process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://paezdev:devenv005@localhost:5432/bank");
 var app = express();
 
 app.use(express.static(__dirname));
@@ -13,3 +15,11 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 3000);
 
 console.log("App running at http://localhost:" + (process.env.PORT ? process.env.PORT : 3000));
+
+db.none('INSERT INTO users(first_name, last_name, username, email, password) VALUES($1, $2, $3, $4, $5)', ['John', 'Smith', 'jsmith', 'jsmith@something.com', 'smith005'])
+    .then(() => {
+        console.log("Success! Item inserted to db.");
+    })
+    .catch(error => {
+        console.log("Sad-face. There was an error. =>" + error);
+    });
