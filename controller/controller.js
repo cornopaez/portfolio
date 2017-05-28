49d4403fa5662e974a7fc538de3634d5d4f80844
Mauricio Paez <cornopaez@gmail.com>
2017-05-28 12:47:01 -0400

    Changed location of SSL enforcement.

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,19 @@
+const express_enforces_ssl = require('express-enforces-ssl');
+
 module.exports = {
 	set: set
 }
 
 function set(app){
+
+	// Check for Prod to enforce SSL
+	if (process.env.NODE_ENV) {
+		app.enable('trust proxy');
+		app.use(express_enforces_ssl());
+		console.log("SSL Enforced")
+	} else {
+		console.log("SSL *not* Enforced")
+	}
 
 	return new Promise(function(resolve, reject) {
 		app.get('/', function (req, res) {
--- node.js
+++ node.js
@@ -2,21 +2,11 @@ const express = require('express');
 const pgp = require('pg-promise')();
 // const db = pgp(process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://paezdev:devenv005@localhost:5432/bank");
 const app = express();
-const express_enforces_ssl = require('express-enforces-ssl');
 const controller = require("./controller/controller.js");
 
 app.use(express.static(__dirname));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
-// Check for Prod to enforce SSL
-if (process.env.NODE_ENV) {
-	app.enable('trust proxy');
-	app.use(express_enforces_ssl());
-	console.log("SSL Enforced")
-} else {
-	console.log("SSL *not* Enforced")
-}
 
 controller.set(app);
 
