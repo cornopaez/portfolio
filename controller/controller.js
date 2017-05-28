3dfc95f55ad9a32d49ecc7ce25891c88a31d5863
Mauricio Paez <cornopaez@gmail.com>
2017-05-28 12:59:35 -0400

    Changed SSL enforcement location again.

--- controller/controller.js
+++ controller/controller.js
@@ -6,15 +6,6 @@ module.exports = {
 
 function set(app){
 
-	// Check for Prod to enforce SSL
-	if (process.env.NODE_ENV) {
-		app.enable('trust proxy');
-		app.use(express_enforces_ssl());
-		console.log("SSL Enforced")
-	} else {
-		console.log("SSL *not* Enforced")
-	}
-
 	return new Promise(function(resolve, reject) {
 		app.get('/', function (req, res) {
 		  res.send('index.html');
--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pgp = require('pg-promise')();
-// const db = pgp(process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://paezdev:devenv005@localhost:5432/bank");
+var http = require('http');
 const app = express();
 const controller = require("./controller/controller.js");
 
@@ -8,8 +8,21 @@ app.use(express.static(__dirname));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// Check for Prod to enforce SSL
+if (process.env.NODE_ENV) {
+	app.enable('trust proxy');
+	app.use(express_enforces_ssl());
+	console.log("SSL Enforced")
+} else {
+	console.log("SSL *not* Enforced")
+}
+
 controller.set(app);
 
-app.listen(process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
 
-console.log("App running at http://localhost:" + (process.env.PORT ? process.env.PORT : 3000));
+// app.listen(process.env.PORT || 3000);
+
+// console.log("App running at http://localhost:" + (process.env.PORT ? process.env.PORT : 3000));
