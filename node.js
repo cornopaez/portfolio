2ed9d88233396abe76c25fbeef8b664700219a61
Mauricio Paez <cornopaez@gmail.com>
2017-05-28 13:05:46 -0400

    Fixed missing port declaration.

--- node.js
+++ node.js
@@ -20,8 +20,8 @@ if (process.env.NODE_ENV) {
 
 controller.set(app);
 
-http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen((process.env.PORT || 3000), function() {
+	console.log('Express server listening on port ' + (process.env.PORT || 3000));
 });
 
 // app.listen(process.env.PORT || 3000);
